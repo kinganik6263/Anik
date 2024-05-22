c7817b568b35b60cb82c373fc2a3d54a5791c614
kinganik6263 <161469632+kinganik6263@users.noreply.github.com>
2024-05-22 18:15:58 +0800

    Update 0repley1.js

--- scripts/commands/0repley1.js
+++ scripts/commands/0repley1.js
@@ -13,7 +13,7 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("Assalamualaikum")==0 || (event.body.indexOf("Assalamu alaikum")==0 || (event.body.indexOf("আসসালামুয়ালাইকুম")==0 || (event.body.indexOf("আসসালামু আলাইকুম")==0)))) {
+	if (event.body.indexOf("Assalamualaikum")==0 || (event.body.indexOf("Assalamu alaikum")==0 || (event.body.indexOf("Asslamulikum")==0 || (event.body.indexOf("আসসালামুয়ালাইকুম")==0 || (event.body.indexOf("আসসালামু আলাইকুম")==0)))) {
 		var msg = {
 				body: "ওয়ালাইকুম সালাম ওয়া রাহমাতুল্লাহি ওয়াবারকাতুহু 🥀🌺:))"
     }
